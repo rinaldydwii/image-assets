d4c97bf5f4635b8f0d06e85f6e66143074a503bc
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-10-31 13:51:32 +0800

    test server

--- index.php
+++ index.php
@@ -9,8 +9,10 @@ $newHeight = isset($_GET['h']) ? $_GET['h'] : '';
 $pathUrl = explode('?', $_SERVER['REQUEST_URI'])[0];
 $file = file_get_contents('..'.$pathUrl);
 echo $file;
+echo "Test";
 
 
 [$width, $height] = getimagesize('../users/avatars/avatar.jpg');
+
 
 // dprintr($size);
