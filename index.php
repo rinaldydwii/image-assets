e5bac23be381c721284da7ffbf2a19eee0e27ecd
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-10-31 18:20:26 +0800

    fix update

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <?php
 header("content-type: image/jpg");
 
-// IMAGE MUST SQUARE
+// Get Path URL
+$pathUrl = ltrim(explode('?', $_SERVER['REQUEST_URI'])[0], '/');
+// Get Image File
+$file = file_get_contents($pathUrl);
 
-$newWidth = isset($_GET['w']) ? $_GET['w'] : '';
-$newHeight = isset($_GET['h']) ? $_GET['h'] : '';
-
-$pathUrl = explode('?', $_SERVER['REQUEST_URI'])[0];
-$file = file_get_contents('users/avatars/1.jpeg');
-echo $file;
-// echo $pathUrl;
-
-
-// [$width, $height] = getimagesize('users/avatars/avatar.jpg');
-
-
-// dprintr($size);
+if ($file) {
+    // Image Size
+    [$width, $height] = getimagesize($pathUrl);
+    // New Image Size
+    $newWidth = isset($_GET['w']) ? $_GET['w'] : $width;
+    $newHeight = isset($_GET['h']) ? $_GET['h'] : $height;
+    // New Image Width or Height if Width or Height is null
+    if ($newWidth) {
+        $scale = $newWidth / $width;
+        $newHeight = $height * $scale;
+    } else if ($newHeight) {
+        $scale = $newHeight / $height;
+        $newWidth = $width * $scale;
+    }
+    // Load Image
+    $imagick = new \Imagick(realpath($pathUrl));
+    // Resize Image
+    $imagick -> resizeImage($newWidth, $newHeight, Imagick::FILTER_POINT, 1);
+    // Return Image
+    echo $imagick->getImageBlob();
+}
